83fb9c5ef22d6de7edc919f11f059efcbd6ce79d
marslanali <arslanali800@gmail.com>
2020-01-18 00:28:09 +0500

    adding additional src files

---
+++ build/CMakeFiles/linear_filter_opencv.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for linear_filter_opencv.
+# This may be replaced when dependencies are built.
---
+++ build/CMakeFiles/template_matching_opencv.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/template_matching_opencv.dir/main.cpp.o"
+  "CMakeFiles/template_matching_opencv.dir/src/video_capture_live_camera.cpp.o"
+  "template_matching_opencv"
+  "template_matching_opencv.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/template_matching_opencv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
